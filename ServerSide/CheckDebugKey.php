676532faca1a458af3e8f022141e494a55a773b7
T.P.N <61611217+LilShieru@users.noreply.github.com>
2020-03-11 20:22:44 +0700

    Fixed bugs.

--- ServerSide/CheckDebugKey.php
+++ ServerSide/CheckDebugKey.php
@@ -4,7 +4,7 @@ if ($_GET["key"]) {
     $debugKey = "password";
     if ($_GET["key"] == $debugKey) {
         echo '
-            <div id="debugMenu"><a href="#" onclick="GetLocalStorage()">Sửa đổi dữ liệu bên trong app</a><br><br>
+            <div id="debugMenu"><a href="#" onclick="GetLocalStorage()" id="editBtn">Sửa đổi dữ liệu bên trong app</a><br><br>
             <div id="localStorage"></div><br>
             <a href="#" onclick="(function () { var script = document.createElement(\'script\'); script.src=\'http://cdn.jsdelivr.net/npm/eruda\'; document.body.appendChild(script); script.onload = function () { eruda.init() } })();">Bật chế độ Kiểm tra phần tử</a>
             </div>
@@ -13,39 +13,31 @@ if ($_GET["key"]) {
                     var html = "";
                     for (var i = 0; i < window.localStorage.length; i++){
                         html += window.localStorage.key(i) + ":<br>";
-                        html += "<input id=\"key" + i + "\" value=\'" + window.localStorage.getItem(window.localStorage.key(i)) + "\' style=\"width:50%;height:40px\" /> <button onclick=\"SaveLocalStorage(" + i + ")\" style=\"width:20%;height:40px\">Lưu lại</button> <button onclick=\"DeleteLocalStorage(" + i + ")\" style=\"width:20%;height:40px\">Xóa</button><br>";
+                        html += "<input id=\"" + window.localStorage.key(i) + "\" value=\'" + window.localStorage.getItem(window.localStorage.key(i)) + "\' style=\"width:50%;height:40px\" /> <button onclick=\"SaveLocalStorage(\'" + window.localStorage.key(i) + "\')\" style=\"width:20%;height:40px\">Lưu lại</button> <button onclick=\"DeleteLocalStorage(\'" + window.localStorage.key(i) + "\')\" style=\"width:20%;height:40px\">Xóa</button><br>";
                         document.getElementById("localStorage").innerHTML = html;
+                        document.getElementById("editBtn").innerHTML = "Làm mới lại dữ liệu bên trong app";
                     }
-                    window.setInterval(function() {
-                        var html = "";
-                        for (var i = 0; i < window.localStorage.length; i++){
-                            html += window.localStorage.key(i) + ":<br>";
-                            html += "<input id=\"key" + i + "\" value=\'" + window.localStorage.getItem(window.localStorage.key(i)) + "\' style=\"width:50%;height:40px\" /> <button onclick=\"SaveLocalStorage(" + i + ")\" style=\"width:20%;height:40px\">Lưu lại</button> <button onclick=\"DeleteLocalStorage(" + i + ")\" style=\"width:20%;height:40px\">Xóa</button><br>";
-                            document.getElementById("localStorage").innerHTML = html;
-                        }
-                    }, 10000);
                 }
-                function SaveLocalStorage(i) {
-                    window.localStorage.setItem(window.localStorage.key(i), document.getElementById("key" + i).value);
-                    alert("Lưu lại dữ liệu trên biến " + window.localStorage.key(i) + " thành công.");
+                function SaveLocalStorage(key) {
+                    window.localStorage.setItem(key, document.getElementById(key).value);
+                    alert("Lưu lại dữ liệu trên biến " + key + " thành công.");
                     var html = "";
                     for (var i = 0; i < window.localStorage.length; i++){
                         html += window.localStorage.key(i) + ":<br>";
-                        html += "<input id=\"key" + i + "\" value=\'" + window.localStorage.getItem(window.localStorage.key(i)) + "\' style=\"width:50%;height:40px\" /> <button onclick=\"SaveLocalStorage(" + i + ")\" style=\"width:20%;height:40px\">Lưu lại</button> <button onclick=\"DeleteLocalStorage(" + i + ")\" style=\"width:20%;height:40px\">Xóa</button><br>";
+                        html += "<input id=\"" + window.localStorage.key(i) + "\" value=\'" + window.localStorage.getItem(window.localStorage.key(i)) + "\' style=\"width:50%;height:40px\" /> <button onclick=\"SaveLocalStorage(\'" + window.localStorage.key(i) + "\')\" style=\"width:20%;height:40px\">Lưu lại</button> <button onclick=\"DeleteLocalStorage(\'" + window.localStorage.key(i) + "\')\" style=\"width:20%;height:40px\">Xóa</button><br>";
                         document.getElementById("localStorage").innerHTML = html;
                     }
                 }
-                function DeleteLocalStorage(i) {
-                    var ans = confirm("Bạn chắc chắn muốn xóa biến \'" + window.localStorage.key(i) + "\' chứ?");
+                function DeleteLocalStorage(key) {
+                    var ans = confirm("Bạn chắc chắn muốn xóa biến \'" + key + "\' chứ?");
                     if (ans) {
-                        var keyName = window.localStorage.key(i);
-                        window.localStorage.removeItem(window.localStorage.key(i));
-                        alert("Xóa biến " + keyName + " thành công.");
+                        window.localStorage.removeItem(key);
+                        alert("Xóa biến " + key + " thành công.");
                         var html = "";
                         for (var i = 0; i < window.localStorage.length; i++){
                             html += window.localStorage.key(i) + ":<br>";
-                            html += "<input id=\"key" + i + "\" value=\'" + window.localStorage.getItem(window.localStorage.key(i)) + "\' style=\"width:50%;height:40px\" /> <button onclick=\"SaveLocalStorage(" + i + ")\" style=\"width:20%;height:40px\">Lưu lại</button> <button onclick=\"DeleteLocalStorage(" + i + ")\" style=\"width:20%;height:40px\">Xóa</button><br>";
-                            document.getElementById("localStorage").innerHTML = html;
+                            html += "<input id=\"" + window.localStorage.key(i) + "\" value=\'" + window.localStorage.getItem(window.localStorage.key(i)) + "\' style=\"width:50%;height:40px\" /> <button onclick=\"SaveLocalStorage(\'" + window.localStorage.key(i) + "\')\" style=\"width:20%;height:40px\">Lưu lại</button> <button onclick=\"DeleteLocalStorage(\'" + window.localStorage.key(i) + "\')\" style=\"width:20%;height:40px\">Xóa</button><br>";
+                        document.getElementById("localStorage").innerHTML = html;
                         }
                     }
                 }
