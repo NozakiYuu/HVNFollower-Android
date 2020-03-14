9b0d24ff764a156d242238bb2cef23c83441addb
T.P.N <61611217+LilShieru@users.noreply.github.com>
2020-03-14 20:42:24 +0700

    Added Tag Filtering feature.

--- ServerSide/API/GetDoujinInfo.php
+++ ServerSide/API/GetDoujinInfo.php
@@ -11,6 +11,18 @@ if ($_GET["id"]) {
    $nodes = $finder->query("//h1[@class='bar-title']");
    $displayName = strtok(substr($nodes[0]->textContent, 33), "Xế");
    $info->displayName = substr($displayName, 0, strlen($displayName) - 1);
+   $content = $finder->query("//li[contains(@class, 'item')][1]//div[contains(@class, 'box-description')][1]");
+   if (strpos($content[0]->textContent, "Tên Khác:") !== false) {
+      $tags = $finder->query("//li[contains(@class, 'item')][1]//div[contains(@class, 'box-description')][1]//p[3]//span//a[contains(@class, 'tag')][1]");
+   }
+   else {
+      $tags = $finder->query("//li[contains(@class, 'item')][1]//div[contains(@class, 'box-description')][1]//p[2]//span//a[contains(@class, 'tag')][1]");
+   }
+   $tagString = "";
+   for ($i = 0; $i < count($tags); $i++){
+      $tagString = $tagString . '' . $tags[$i]->textContent . ',';
+   }
+   $info->tags = substr($tagString, 0, strlen($tagString) - 1);
    $info->comicLink = "https://hentaivn.net" . $finder->evaluate("string(//div[@class='box-description']//p[@style='font-size: 18px; line-height: 22px;']//a/@href)");
    $truyen_list = $finder->query("//div[@class='box-description']//p[@style='font-size: 18px; line-height: 22px;']");
    $info->firstComic = substr($truyen_list[0]->textContent, 1);
