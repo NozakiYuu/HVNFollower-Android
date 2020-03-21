0707a6b555c4ede9b93790b858d6e44a04b7cf51
T.P.N <61611217+LilShieru@users.noreply.github.com>
2020-03-21 10:13:04 +0700

    Fixed bugs related to the Tag-Filtering feature.

--- ServerSide/API/GetDoujinInfo.php
+++ ServerSide/API/GetDoujinInfo.php
@@ -20,7 +20,7 @@ if ($_GET["id"]) {
    }
    $tagString = "";
    for ($i = 0; $i < count($tags); $i++){
-      $tagString = $tagString . '' . $tags[$i]->textContent . ',';
+      $tagString = $tagString . '' . str_replace(" ", "", $tags[$i]->textContent) . ',';
    }
    $info->tags = substr($tagString, 0, strlen($tagString) - 1);
    $info->comicLink = "https://hentaivn.net" . $finder->evaluate("string(//div[@class='box-description']//p[@style='font-size: 18px; line-height: 22px;']//a/@href)");
